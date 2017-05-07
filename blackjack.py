e2201fb96619c39150d7899d2d60e9bcd521ec02
Megan <megan.e.dose@gmail.com>
2017-05-06 20:28:06 -0700

    created deck and shuffle func

---
+++ blackjack.py
@@ -0,0 +1,74 @@
+import random
+
+def make_new_deck():
+    # returns new deck
+    return [
+        (2, "diamonds"),
+        (3, "diamonds"),
+        (4, "diamonds"),
+        (5, "diamonds"),
+        (6, "diamonds"),
+        (7, "diamonds"),
+        (8, "diamonds"),
+        (9, "diamonds"),
+        (10, "diamonds"),
+        ("jack", "diamonds"),
+        ("queen", "diamonds"),
+        ("king", "diamonds"),
+        ("ace", "diamonds"),
+        (2, "clubs"),
+        (3, "clubs"),
+        (4, "clubs"),
+        (5, "clubs"),
+        (6, "clubs"),
+        (7, "clubs"),
+        (8, "clubs"),
+        (9, "clubs"),
+        (10, "clubs"),
+        ("jack", "clubs"),
+        ("queen", "clubs"),
+        ("king", "clubs"),
+        ("ace", "clubs"),
+        (2, "hearts"),
+        (3, "hearts"),
+        (4, "hearts"),
+        (5, "hearts"),
+        (6, "hearts"),
+        (7, "hearts"),
+        (8, "hearts"),
+        (9, "hearts"),
+        (10, "hearts"),
+        ("jack", "hearts"),
+        ("queen", "hearts"),
+        ("king", "hearts"),
+        ("ace", "hearts"),
+        (2, "spades"),
+        (3, "spades"),
+        (4, "spades"),
+        (5, "spades"),
+        (6, "spades"),
+        (7, "spades"),
+        (8, "spades"),
+        (9, "spades"),
+        (10, "spades"),
+        ("jack", "spades"),
+        ("queen", "spades"),
+        ("king", "spades"),
+        ("ace", "spades")
+    ]
+
+def shuffle(deck):
+    # shuffle the deck(list of tuples, each tuple has a rank, and a suit) -> returns None
+    random.shuffle(deck)
+
+# def deal
+
+def execute_repl(deck):
+    #user interface (menu choices)
+    pass
+
+deck = make_new_deck()
+execute_repl(deck)
+
+
+# deck -> list, player -> list, dealer -> list; move itmes from one list to another
