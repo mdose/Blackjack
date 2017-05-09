7b7639a92d80c31c8a5e7e1c4fb9301326774354
Megan <megan.e.dose@gmail.com>
2017-05-08 20:24:27 -0700

    first draft of intial deals to player and dealer

--- blackjack.py
+++ blackjack.py
@@ -61,24 +61,44 @@ def shuffle(deck):
     # shuffle the deck(list of tuples, each tuple has a rank, and a suit) -> returns None
     random.shuffle(deck)
 
-def deal():
-    pass
+def deal(deck, hand):
+    # remove first four cards; one to player (list); one to dealer(list)
+    #player.append(deck.pop(0))
+    top_card = deck[0]
+    deck.remove(top_card)
+    hand.append(top_card)
 
-def score():
-    pass
+# def assess_score(score, hand):
+    #convert face cards to 10 in tuples
+    #add first part of card tuples together
+    #have ace be either 1 or 11 depending on total score
+    # if card in hand[0][0] ==
 
-def hit():
-    pass
 
-def dealer_plays():
-    pass
+
+#def hit():
+    #pass
+
+#def dealer_plays():
+    #pass
 
 def execute_repl(deck):
     #user interface (menu choices)
     pass
 
 deck = make_new_deck()
-execute_repl(deck)
+shuffle(deck)
+player = []
+dealer = []
+deal(deck, player)
+deal(deck, dealer)
+deal(deck, player)
+deal(deck, dealer)
+#assess_score(score, hand)
+
+
+
+# execute_repl(deck)
 
 
 # deck -> list, player -> list, dealer -> list; move itmes from one list to another
